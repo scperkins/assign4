e4fd1c88d709f941e8022752e83402ee1634920e
Sean Perkins <perkinsean@gmail.com>
2012-11-20 16:40:22 -0500

    created top for head section of index

---
+++ top.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Michael Perkins</title>
+<meta charset="utf-8">
+<meta name="author" content="Sean Perkins">
+<meta name="description" content="This is the home page for the website of Michael Perkins">
+
+<link rel="stylesheet"
+href="style.css"
+type="text/css"
+media="screen">
+
+</head>
