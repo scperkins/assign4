00ab3792e4c385b6bebd658b21cda376d7fcc546
Sean Perkins <perkinsean@gmail.com>
2012-11-20 16:51:00 -0500

    created menu module, edited style, index

---
+++ menu.php
@@ -0,0 +1,10 @@
+<nav> 
+	<ol>
+		<li><a href="#">Resume</li>
+		<li><a href="#">About Me</li>
+		<li><a href="#">Partnerships</li>
+		<li><a href="#">Contact Me</li>
+		<li><a href="#">Projects</li>
+		<li><a href="#">References</li>
+	</ol>
+</nav>
