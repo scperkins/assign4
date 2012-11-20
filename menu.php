89b16f2131c94386a18fd0cb7f96ffc829f843e9
Sean Perkins <perkinsean@gmail.com>
2012-11-20 18:38:07 -0500

    css changes, added table

--- menu.php
+++ menu.php
@@ -1,10 +1,12 @@
 <nav> 
-	<ul>
-		<li><a href="#">Resume</a></li>
-		<li><a href="#">About Me</a></li>
-		<li><a href="#">Partnerships</a></li>
-		<li><a href="#">Contact Me</a></li>
-		<li><a href="#">Projects</a></li>
-		<li><a href="#">References</a></li>
-	</ul>
+	<table>
+		<tr>
+		<td><a href="#">Resume</a></td> 
+		<td><a href="#">About Me</a></td> 
+		<td><a href="#">Partnerships</a></td> 
+		<td><a href="#">Contact Me</a></td> 
+		<td><a href="#">Projects</a></td> 
+		<td><a href="#">References</a></td> 
+		</tr>
+		</table>
 </nav>
