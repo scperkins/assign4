1b5f12d210fa61896a3e5d382e24332b2505bf9d
Sean Perkins <perkinsean@gmail.com>
2012-11-20 22:49:50 -0500

    some css improvments, finally

--- sideMenu.php
+++ sideMenu.php
@@ -1,6 +1,6 @@
 
 <ul class="sideMenu">
-	<li><h2>Detailed Information</h2></li>
+	<li><h3>Detailed Information</h3></li>
 	<li><a href="#">Skills</a></li>
 	<li><a href="#">Education</a></li>
 	<li><a href="#">Experience</a></li>
