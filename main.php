3b57f8cd5b90302df8b1d0b2694a7f99c778dfae
Sean Perkins <perkinsean@gmail.com>
2012-11-20 19:14:05 -0500

    adding main and css files again

--- main.php
+++ main.php
@@ -4,10 +4,11 @@
 <? include "menu.php" ?>
 <div><?include "sideMenu.php" ?></div>
 <!--Main Content Wrapper -->
-<div>
-
+<div id="center">
 
 <p>What ever you want to say goes here</p>
+
 </div>
+<? include "footer.php" ?>
 </body>
 </html>
