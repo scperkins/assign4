b9cb0bda0e41ae4a33160991b60f044a5793d180
Sean Perkins <perkinsean@gmail.com>
2012-11-20 21:38:58 -0500

    minor css changes

--- main.php
+++ main.php
@@ -4,11 +4,15 @@
 <? include "header.php" ?>
 <div><img class="mike" src="images/mike.jpg" height="220" width="240"></img></div>
 <? include "menu.php" ?>
-<div><?include "sideMenu.php" ?></div>
-<!--Main Content Wrapper -->
-<div id="center">
+<?include "sideMenu.php" ?>
 
-<p>What ever you want to say goes here</p>
+<!--Main Content Wrapper -->
+<div>
+	<h2>Objective</h2>
+		<p>Seek professional employment to offer leadership and enhance a growing company's potential.</p>
+
+		<h3>Skills</h3>
+			<p>Manufacturing / Engineering Process and Control, CAD, CAD/CAM, Design, Estimating, Building, Woodworking, Metalworking, Mechanics, Project Management, Production Cost Analysis, Computer Networking, Linux, Microsoft, AutoCAD, Architectural Woodworking, Construction, Electrical and Electronics Processes and Control, CNC, Customer Relations and Corporate Management.</p>
 
 </div>
 <? include "footer.php" ?>
--- sideMenu.php
+++ sideMenu.php
@@ -1,11 +1,10 @@
-<div>
+
 <ul class="sideMenu">
 	<li><h2>Detailed Information</h2></li>
-	<li><a href="#"></a>Skills</li>
-	<li><a href="#"></a>Education</li>
-	<li><a href="#"></a>Experience</li>
-	<li><a href="#"></a>Interests</li>
-	<li><a href="#"></a>Affiliations and Community Service</li>
-	<li><a href="#"></a>References</li>
+	<li><a href="#">Skills</a></li>
+	<li><a href="#">Education</a></li>
+	<li><a href="#">Experience</a></li>
+	<li><a href="#">Interests</a></li>
+	<li><a href="#">Affiliations and Community Service</a></li>
+	<li><a href="#">References</a></li>
 </ul>
-</div>
