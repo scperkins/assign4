dbf1bcdfb5fe440ad4f3b1964d58249509beee7f
Sean Perkins <perkinsean@gmail.com>
2012-11-20 19:12:56 -0500

    added footer, more css changes

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+	
+	<p>Website designed by Sean Perkins, <span><script language="JavaScript">
+		var d = new Date();
+		yr = d.getFullYear();
+		document.write(yr);
+	 </script></span></p>
+</footer>
